c337d752ffabfa3b3bbe0a8e0c2fecdd27051ce2
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2022-11-21 15:56:31 +0100

    Creating MultiThreadedSumMatrix class

---
+++ 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -0,0 +1,22 @@
+package it.unibo.oop.workers02;
+
+public class MultiThreadedSumMatrix implements SumMatrix {
+
+    private final int numThread;
+
+    /**
+     * 
+     * @param numThread
+     *            no. of thread performing the sum.
+     */
+    public MultiThreadedSumMatrix(final int numThread) {
+        this.numThread = numThread;
+    }
+
+    @Override
+    public double sum(double[][] matrix) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
